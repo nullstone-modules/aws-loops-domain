5f96a5e61c3ae73d3d9d386e2dbc819fc2c3f8f0
Brad Sickles <brad.sickles@gmail.com>
2023-11-02 09:58:07 -0400

    fix mx record

--- mx.tf
+++ mx.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "mail_from" {
   zone_id = local.domain_zone_id
   name    = "envelope"
-  type    = "CNAME"
+  type    = "MX"
   ttl     = 3600
   records = ["feedback-smtp.us-east-1.amazonses.com."]
 }
